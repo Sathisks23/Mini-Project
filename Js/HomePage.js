54970735d81740794386e610668a2c2c6f94765d
DCKAP <sathishksdckap@gmail.com>
2024-02-27 12:34:45 +0530

    updated mster

--- Js/HomePage.js
+++ Js/HomePage.js
@@ -45,7 +45,6 @@ let db = getFirestore(app)
 
 
 
-
 //for see more ...
 let see_more = document.querySelectorAll('#see_more')
     see_more.forEach(item=>{
@@ -162,90 +161,158 @@ let i=0
     
    console.log(fav_arry);
 
-   for(i in fav_arry){
+//    for(i in fav_arry){
 
    
 
-        let post_getref =  collection(db,'post')
-       let q = query(post_getref,where('c_name','==',fav_arry[i]))
+//         let post_getref =  collection(db,'post')
+//        let q = query(post_getref,where('c_name','==',fav_arry[i]))
 
-        let querysap = await getDocs(q)
+//         let querysap = await getDocs(q)
         
-       querysap.forEach(async(rec)=>{
+//        querysap.forEach(async(rec)=>{
           
-        let uid =rec.data().u_id
+//         let uid =rec.data().u_id
 
-        let    mul_user_getref = doc(db,'user',uid)
-        let   mu_user_getData  =await  getDoc(mul_user_getref)
-            mu_user_getData.data().u_id
+//         let    mul_user_getref = doc(db,'user',uid)
+//         let   mu_user_getData  =await  getDoc(mul_user_getref)
+//             mu_user_getData.data().u_id
 
-    let card = document.createElement('div')
-        card.className = 'post_view'
+//     let card = document.createElement('div')
+//         card.className = 'post_view'
 
-        card.innerHTML = 
-       ` <div class='post_head'> <img id='user_dp' src='${ mu_user_getData.data().u_dp}'> <h4>'${mu_user_getData.data().u_name}'</h4></div>`+
-        `<div class='description'><p id='post_desc'>'${rec.data().u_desc}'</p> </div>`+
+//         card.innerHTML = 
+//        ` <div class='post_head'> <img id='user_dp' src='${ mu_user_getData.data().u_dp}'> <h4>'${mu_user_getData.data().u_name}'</h4></div>`+
+//         `<div class='description'><p id='post_desc'>'${rec.data().u_desc}'</p> </div>`+
         
-       ` <div class='post_div'><img id='post' src='${rec.data().p_link}'></div>`+
-       ` <div class='social_section'><div class='like_div'> <i onclick='uplike(this)' id='check_like'  class='fa-regular fa-thumbs-up'></i>  <b  id='likes'>0</b> </div> <div class='comment_icon'> <i onclick='showcomment(this)'' class='fa-regular fa-comment'></i>  <b id='comment_counnt'>100</b></div> </div>`
-        "<div class=;comment_section'>"
-            "<div class='add_comment_section'> <img id='comment_dp' src='' ><input onkeyup='sendshow(this)' id='comment_input' placeholder='add a comment..' type='text'> <i id='sending' class='fa-regular fa-paper-plane'></i> <i   class='fa-regular fa-face-smile'></i></div>"
-            " <div class='comments_list'>"
-                "<div class='comment_div'> <img  id='cmnt_user_dp' src='' > <div class='cmnt_header'><p id='comment'>Yeah actually that is true,there many trees that have been cutting down we have to grow some trees in free space.</p><div class='replay'><i class='fa-solid fa-reply'></i><span class='replay_s'>replay</span></div></div> </div>"
-             "</div>"
-        "</div>"
-    
-
-main_view.append(card)
-        
-        })
-
-    }
-
-   async function category_selected(element){
-       
-          let post_getref =  collection(db,'post')
-       let q = query(post_getref,where('c_name','==',element.innerText))
-       
-        let querysap = await getDocs(q)
-        
-       querysap.forEach(async(rec)=>{
-          
-        let uid =rec.data().u_id
-
-        let    mul_user_getref = doc(db,'user',uid)
-        let   mu_user_getData  =await  getDoc(mul_user_getref)
-            mu_user_getData.data().u_id
-
-    let card = document.createElement('div')
-        card.className = 'post_view'
-
-        card.innerHTML = 
-       ` <div class='post_head'> <img id='user_dp' src='${ mu_user_getData.data().u_dp}'> <h4>'${mu_user_getData.data().u_name}'</h4></div>`+
-        `<div class='description'><p id='post_desc'>'${rec.data().u_desc}'</p> </div>`+
-        
-       ` <div class='post_div'><img id='post' src='${rec.data().p_link}'></div>`+
-       ` <div class='social_section'><div class='like_div'> <i onclick='uplike(this)' id='check_like'  class='fa-regular fa-thumbs-up'></i>  <b  id='likes'>0</b> </div> <div class='comment_icon'> <i onclick='showcomment(this)'' class='fa-regular fa-comment'></i>  <b id='comment_counnt'>100</b></div> </div>`
-        "<div class=;comment_section'>"
-            "<div class='add_comment_section'> <img id='comment_dp' src='' ><input onkeyup='sendshow(this)' id='comment_input' placeholder='add a comment..' type='text'> <i id='sending' class='fa-regular fa-paper-plane'></i> <i   class='fa-regular fa-face-smile'></i></div>"
-            " <div class='comments_list'>"
-                "<div class='comment_div'> <img  id='cmnt_user_dp' src='' > <div class='cmnt_header'><p id='comment'>Yeah actually that is true,there many trees that have been cutting down we have to grow some trees in free space.</p><div class='replay'><i class='fa-solid fa-reply'></i><span class='replay_s'>replay</span></div></div> </div>"
-             "</div>"
-        "</div>"
+//        ` <div class='post_div'><img id='post' src='${rec.data().p_link}'></div>`+
+//        ` <div class='social_section'><div class='like_div'> <i onclick='uplike(this)' id='check_like'  class='fa-regular fa-thumbs-up'></i>  <b  id='likes'>0</b> </div> <div class='comment_icon'> <i onclick='showcomment(this)'' class='fa-regular fa-comment'></i>  <b id='comment_counnt'>100</b></div> </div>`
+//         "<div class=;comment_section'>"
+//             "<div class='add_comment_section'> <img id='comment_dp' src='' ><input onkeyup='sendshow(this)' id='comment_input' placeholder='add a comment..' type='text'> <i id='sending' class='fa-regular fa-paper-plane'></i> <i   class='fa-regular fa-face-smile'></i></div>"
+//             " <div class='comments_list'>"
+//                 "<div class='comment_div'> <img  id='cmnt_user_dp' src='' > <div class='cmnt_header'><p id='comment'>Yeah actually that is true,there many trees that have been cutting down we have to grow some trees in free space.</p><div class='replay'><i class='fa-solid fa-reply'></i><span class='replay_s'>replay</span></div></div> </div>"
+//              "</div>"
+//         "</div>"
     
 
 // main_view.append(card)
+        
+//         })
 
-    }
+//     }
+
+    //  Categories
+
+ 
+   async function category_selected(element){
+
+      
+
+
+      
+    if(element.innerText!='My favourtie' && element!=0){
+        let post_getref =  collection(db,'post')
+        let q = query(post_getref,where('c_name','==',element.innerText))
+        
+         let querysap = await getDocs(q)
+         
+        querysap.forEach(async(rec)=>{
+           
+           console.log(rec.data());
+         let uid =rec.data().u_id
+
+         
+ 
+         let    mul_user_getref = doc(db,'user',uid)
+         let   mu_user_getData  =await  getDoc(mul_user_getref)
+             mu_user_getData.data().u_id
+ 
+     let card = document.createElement('div')
+         card.className = 'post_view'
+
+         main_view.innerHTML = ''
+ 
+         card.innerHTML = 
+        ` <div class='post_head'> <img id='user_dp' src='${ mu_user_getData.data().u_dp}'> <h4>'${mu_user_getData.data().u_name}'</h4></div>`+
+         `<div class='description'><p id='post_desc'>'${rec.data().u_desc}'</p> </div>`+
+         
+        ` <div class='post_div'><img id='post' src='${rec.data().p_link}'></div>`+
+        ` <div class='social_section'><div class='like_div'> <i onclick='uplike(this)' id='check_like'  class='fa-regular fa-thumbs-up'></i>  <b  id='likes'>0</b> </div> <div class='comment_icon'> <i onclick='showcomment(this)'' class='fa-regular fa-comment'></i>  <b id='comment_counnt'>100</b></div> </div>`
+         "<div class=;comment_section'>"
+             "<div class='add_comment_section'> <img id='comment_dp' src='' ><input onkeyup='sendshow(this)' id='comment_input' placeholder='add a comment..' type='text'> <i id='sending' class='fa-regular fa-paper-plane'></i> <i   class='fa-regular fa-face-smile'></i></div>"
+             " <div class='comments_list'>"
+                 "<div class='comment_div'> <img  id='cmnt_user_dp' src='' > <div class='cmnt_header'><p id='comment'>Yeah actually that is true,there many trees that have been cutting down we have to grow some trees in free space.</p><div class='replay'><i class='fa-solid fa-reply'></i><span class='replay_s'>replay</span></div></div> </div>"
+              "</div>"
+         "</div>"
+
+
+main_view.append(card)
+
+     
+    }    )
+
+// My favourite 
+       
+         }else {
+
+            main_view.innerHTML = ''
+
+            for(i in fav_arry){
+
+   
+
+                let post_getref =  collection(db,'post')
+               let q = query(post_getref,where('c_name','==',fav_arry[i]))
+        
+                let querysap = await getDocs(q)
+                
+               querysap.forEach(async(rec)=>{
+                  
+                let uid =rec.data().u_id
+        
+                let    mul_user_getref = doc(db,'user',uid)
+                let   mu_user_getData  =await  getDoc(mul_user_getref)
+                    mu_user_getData.data().u_id
+        
+            let card = document.createElement('div')
+                card.className = 'post_view'
+        
+                card.innerHTML = 
+               ` <div class='post_head'> <img id='user_dp' src='${ mu_user_getData.data().u_dp}'> <h4>'${mu_user_getData.data().u_name}'</h4></div>`+
+                `<div class='description'><p id='post_desc'>'${rec.data().u_desc}'</p> </div>`+
+                
+               ` <div class='post_div'><img id='post' src='${rec.data().p_link}'></div>`+
+               ` <div class='social_section'><div class='like_div'> <i onclick='uplike(this)' id='check_like'  class='fa-regular fa-thumbs-up'></i>  <b  id='likes'>0</b> </div> <div class='comment_icon'> <i onclick='showcomment(this)'' class='fa-regular fa-comment'></i>  <b id='comment_counnt'>100</b></div> </div>`
+                "<div class=;comment_section'>"
+                    "<div class='add_comment_section'> <img id='comment_dp' src='' ><input onkeyup='sendshow(this)' id='comment_input' placeholder='add a comment..' type='text'> <i id='sending' class='fa-regular fa-paper-plane'></i> <i   class='fa-regular fa-face-smile'></i></div>"
+                    " <div class='comments_list'>"
+                        "<div class='comment_div'> <img  id='cmnt_user_dp' src='' > <div class='cmnt_header'><p id='comment'>Yeah actually that is true,there many trees that have been cutting down we have to grow some trees in free space.</p><div class='replay'><i class='fa-solid fa-reply'></i><span class='replay_s'>replay</span></div></div> </div>"
+                     "</div>"
+                "</div>"
+            
+        
+
+        
+        main_view.append(card)
+                
+                })
+        
+            }
+        
 
 
 
 
-       )}
+
+         }
 
 
 
-// let getData = await getDocs(getref)
-//    getData.forEach((record)=>{
-//     console.log(record.data().u_name);
-//    })
+
+
+
+}
+
+
+
+
